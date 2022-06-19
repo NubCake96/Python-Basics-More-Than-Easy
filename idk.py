d0c566ed0c6cd37219a87fc5cdf8cf0185747f5c
Ham-Mad <hammaadulbari@gmail.com>
2022-06-19 16:54:02 +0600

    first commit

---
+++ idk.py
@@ -0,0 +1,10 @@
+#Question 1: 
+#Write a program that prints your name"
+
+my_name = 'Hammaad'
+print(my_name)
+
+#Question 2: 
+#Write a program that takes input your age and shows it in output. 
+age = input(int('Your age is :'))
+print('hello,' +age)
